e1758f6c8d83373b5566423286e56a19511ddac4
DomesanSampath <sdomess@gmail.com>
2016-12-18 09:58:39 -0800

    Update string_to_int.py

--- string_to_int.py
+++ string_to_int.py
@@ -2,8 +2,10 @@
 x=input("Enter a String:")
 y=[]
 count=0
+#Getting String in List
 for i in range(0,len(x)):
     y.append(x[i])
+#Printing ASCII or INT value for each character
 for j in range(0,len(y)):
-    count=+ord(y[j])
+    count=count+ord(y[j])
 print(count)
