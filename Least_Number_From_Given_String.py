a7adfacb1e34dce85b9cc4124ae88b495bdf9c19
DomesanSampath <sdomess@gmail.com>
2017-01-04 15:00:02 +0530

    Update Least_Number_From_Given_String.py

--- Least_Number_From_Given_String.py
+++ Least_Number_From_Given_String.py
@@ -1,2 +1,11 @@
 x=input('Enter a Number')
+y=[]
 for i in range(0,len(x)):
+  y.append(x[i])
+y.sort()
+dom=''
+for i in range(0,len(y)+1):
+  del(y[0])
+  for j in range(0,len(y)):
+    dom=dom+y[j]
+    print('After Deleting %d Digit: %d'(%i,%int(dom)))
