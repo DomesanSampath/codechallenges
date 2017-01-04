d739b2210da0ab71b4e9e1cc2653f41d5a11de2b
DomesanSampath <sdomess@gmail.com>
2017-01-04 06:26:27 +0530

    Update and rename Finding_Unique_Element_from_Twice_Element_Array.py to Finding_Unique_Element_from_Twice_Element_Array_Algorithim.py

--- Finding_Unique_Element_from_Twice_Element_Array_Algorithim.py
+++ Finding_Unique_Element_from_Twice_Element_Array_Algorithim.py
@@ -1,4 +1,8 @@
 #From the Twice Repeated Element Array Finding the Unique Element
+#0.For Iteration for Length of Given Array
+#1. Each Time Get the No. Of counts of Array Element
+#2. Each Time Check for Count==1
+#3. If count==1, Get the Number It will be unique
 x=input('Enter an Array Elements: Eg. 1,2,3,4::')
 x=list(x)
 count=0
